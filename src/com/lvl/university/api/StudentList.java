10e1179b654b8e0b2c0d973d5500b666495771ae
LVL <luis.vinelerma@telefonica.com>
2023-05-04 11:27:39 +0200

    Create StudentList interface

---
+++ src/com/lvl/university/api/StudentList.java
@@ -0,0 +1,10 @@
+package com.lvl.university.api;
+
+import com.lvl.university.model.Student;
+
+public interface StudentList {
+	
+	public void add(Student student);
+	public void delete(Student student);
+
+}
